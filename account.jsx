092584fb3a8d6d8f652a6ce8b222d6769dc1d68f
Tyler <tylerclancy030@gmail.com>
2022-01-11 18:29:05 -0600

    Create function component with basic form

---
+++ account.jsx
@@ -0,0 +1,11 @@
+const Account = () => {
+  return (
+    <form>
+      <input type="number" />
+      <input type="submit" />
+    </form>
+  );
+};
+
+// ==========================================
+ReactDOM.render(<Account />, document.getElementById('root'));
